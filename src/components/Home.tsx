74bb5936bbdd2399f67c73efcb997d707f0b9e83
M0hamady <119875626+M0hamady@users.noreply.github.com>
2024-07-02 18:23:39 +0300

    update loading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -59,7 +59,7 @@ const Home: React.FC = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       handleTasks();
-    }, 4000); // Run every second (1000 milliseconds)
+    }, 400000); // Run every second (1000 milliseconds)
 
     return () => {
       clearInterval(interval); // Clean up the interval on component unmount
